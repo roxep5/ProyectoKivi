38c60efbd8565227e207dd8ad4e5803d844b2552
David <nekosukeneko000@gmail.com>
2021-03-03 13:43:20 +0100

    0.1.7

--- MenuSelect.py
+++ MenuSelect.py
@@ -17,45 +17,72 @@ from kivy.clock import Clock
 
 
 
-class MyApp(App):
+class Vistas(BoxLayout):
+    vistaMenu=BoxLayout()
+    VistaSelect=BoxLayout(orientation='vertical', spacing=20, padding=50)
+    VistasFotos=BoxLayout(orientation='vertical', spacing=20, padding=50)
+
+    def __init__(self, **kwargs):
+        super(Vistas, self).__init__(**kwargs)
+
+        self.vistaMenu=BoxLayout(orientation='vertical', spacing=20, padding=50)
+
+        self.lbl = Label(text='BIENVENIDO/A A AVALANCHE', color=(1.5, 1.35, 0.9, 2), font_size=45)
+        self.btn = Button(text='Empezar', size_hint=(0.17, 0.3), pos_hint={'center_x': 0.5}, color=(1.5, 1.6, 0.9, 2))
 
 
-
-    def build(self):
-        layout = BoxLayout(orientation='vertical', spacing=20, padding=50)
-
-        with layout.canvas:
-            self.rect = Rectangle(source='Imagenes/bosque.png')
-        layout.bind(on_size=self.update)
+        with self.canvas:
+            self.rect = Rectangle(pos=self.pos, size=self.size,source='Imagenes/Avalanche.png')
 
 
-        lbl = Label(text='Elija un tipo de presentacion',color= (1.5,1.35,0.9,2),font_size= 30)
-        btn = Button(text='<- Salir', size_hint=(0.17,0.25),pos_hint= {'center_x': 0.1},color= (1.5,1.6,0.9,2))
-        btn1 = Button(text='Recuerdos con personas',size_hint=(0.3,0.3),pos_hint= {'center_x': 0.5},color= (1.5,1.6,0.9,2))
-        btn2 = Button(text='Recuerdos de un lugar', size_hint=(0.3, 0.3), pos_hint={'center_x': 0.5}, color=(1.5, 1.6, 0.9, 2))
-        btn3 = Button(text='Recuerdos de un evento', size_hint=(0.3, 0.3), pos_hint={'center_x': 0.5}, color=(1.5, 1.6, 0.9, 2))
-        btn4 = Button(text='Continuar ->', size_hint=(0.17, 0.25), pos_hint={'center_x': 0.9}, color=(1.5, 1.6, 0.9, 2))
-
-        layout.add_widget(btn)
-        layout.add_widget(lbl)
-
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)
-        layout.add_widget(btn3)
-        layout.add_widget(btn4)
-
-        Clock.schedule_once(self.update, -1)
+        self.btn.bind(on_press=self.Seleccionar)
 
 
+        self.vistaMenu.add_widget(self.lbl)
+        self.vistaMenu.add_widget(self.btn)
 
-        return layout
+        self.add_widget(self.vistaMenu)
 
-    def update(self, *args):
-        # set the size and position of the background image
-        self.rect.size = self.root.size
-        self.rect.pos = self.root.pos
+    def Seleccionar(self, *args):
+        self.remove_widget(self.seleccionarMenu)
+
+        self.lbl = Label(text='¿Que tipo de presentacion deseas?', color=(1.5, 1.35, 0.9, 2), font_size=30)
+        self.btn1 = Button(text='Personas', size_hint=(0.35, 0.3), pos_hint={'center_x': 0.5},
+                      color=(1.5, 1.6, 0.9, 2))
+        self.btn2 = Button(text='Lugares', size_hint=(0.35, 0.3), pos_hint={'center_x': 0.5},
+                      color=(1.5, 1.6, 0.9, 2))
+        self.btn3 = Button(text='Eventos', size_hint=(0.35, 0.3), pos_hint={'center_x': 0.5},
+                           color=(1.5, 1.6, 0.9, 2))
+        self.btn4 = Button(text='Continuar ->', size_hint=(0.17, 0.25), pos_hint={'center_x': 0.9}, color=(1.5, 1.6, 0.9, 2))
+
+        with self.canvas:
+            self.rect = Rectangle(pos=self.pos, size=self.size,source='Imagenes/bosque.png')
 
 
+        self.btn1.bind(on_press=self.Seleccionar)
+        self.btn2.bind(on_press=self.Seleccionar)
+        self.btn3.bind(on_press=self.Seleccionar)
+        self.vistaSelect.add_widget(self.lbl)
+        self.vistaSelect.add_widget(self.btn1)
+        self.vistaSelect.add_widget(self.btn2)
+        self.vistaSelect.add_widget(self.btn3)
+        self.vistaSelect.add_widget(self.btn4)
+        self.add_widget(self.vistaSelect)
 
+    def Fotos(self, *args):
+        self.remove_widget(self.seleccionarMenu)
 
-MyApp().run()
+        self.img = Image(source='Imagenes/icono.png', pos_hint={'center_x': 0.5})
+
+        self.btn1 = Button(text='Elija sus fotos', size_hint=(0.35, 0.3), pos_hint={'center_x': 0.5},
+                      color=(1.5, 1.6, 0.9, 2))
+        self.btn2 = Button(text='Ver numero de fotos seleccionadas', size_hint=(0.35, 0.3), pos_hint={'center_x': 0.5}, color=(1.5, 1.6, 0.9, 2))
+        self.btn4 = Button(text='Continuar ->', size_hint=(0.17, 0.25), pos_hint={'center_x': 0.9}, color=(1.5, 1.6, 0.9, 2))
+
+        self.vistaFotos.add_widget(self.img)
+        self.vistaFotos.add_widget(self.btn1)
+        self.vistaFotos.add_widget(self.btn2)
+        self.vistaFotos.add_widget(self.btn3)
+        self.vistaFotos.add_widget(self.btn4)
+
+Vistas().run()
